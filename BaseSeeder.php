e06f29a783f5b2f9f2b15c12cbff25f4d9d843e0
basic-app <dev@basic-app.com>
2021-03-02 17:42:06 +0300

    n/a

---
+++ BaseSeeder.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author Basic App Dev Team
+ * @license MIT
+ * @link https://basic-app.com
+ */
+namespace BasicApp\Seeder;
+
+abstract class BaseSeeder extends \CodeIgniter\Database\Seeder
+{
+}
